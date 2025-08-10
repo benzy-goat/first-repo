bfe35a6a02a1a24af8e17d8d5f2616a0ab9aae59
Mohmed Albakro <mohamadalbakro104@gmial.com>
2025-08-10 17:59:05 +0300

    foot controller

---
+++ app/Http/Controllers/FootballController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FootballController extends Controller
+{
+    public function he (){
+        echo 'hello from foot';
+    }
+}
